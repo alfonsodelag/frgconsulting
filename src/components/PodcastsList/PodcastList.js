f7ceb88d1225edf446a1e02dfff8b845a050e9e5
alfonso <alfonsodelag1@gmail.com>
2023-08-30 20:26:00 +0200

    feat(PodcastList): Enhance podcast play/pause functionality
    - Implement play/pause toggle functionality for podcast items and the player controls.
    - Replace play button image with `CiPlay1` and `CiPause1` icons for improved UI consistency.
    - Handle podcast play logic ensuring only one podcast plays at a time.
    - Provide error handling for audio playback issues.

--- src/components/PodcastsList/PodcastList.js
+++ src/components/PodcastsList/PodcastList.js
@@ -1,9 +1,14 @@
 import React, { useRef, useState } from "react";
 import { AiFillPauseCircle, AiFillPlayCircle } from "react-icons/ai";
-import { CiPause1, CiPlay1 } from "react-icons/ci";
+import { CiPause1, CiPlay1, CiVolumeHigh } from "react-icons/ci";
 import ArrowDown from "../../assets/png/arrow-down.png";
 import Clock from "../../assets/png/clock.png";
 import SearchIcon from "../../assets/png/search-icon.png";
+import Loop from "../../assets/png/loop.png";
+import Play from "../../assets/png/play.png";
+import Shuffle from "../../assets/png/shuffle.png";
+import StepBack from "../../assets/png/step-back.png";
+import StepForward from "../../assets/png/step-forward.png";
 import Spinner from "../Spinner/Spinner";
 
 const PodcastList = ({ filteredPodcasts, isLoading }) => {
@@ -12,6 +17,8 @@ const PodcastList = ({ filteredPodcasts, isLoading }) => {
     trackId: null,
   });
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
   const audioRefs = useRef({});
 
   const togglePlayPause = (id) => {
@@ -42,6 +49,12 @@ const PodcastList = ({ filteredPodcasts, isLoading }) => {
   const currentlyPlayingPodcast = filteredPodcasts?.find(
     (podcast) => podcast.trackId === currentPlaying.trackId,
   );
+
+  function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+  }
 
   return (
     <>
@@ -111,6 +124,8 @@ const PodcastList = ({ filteredPodcasts, isLoading }) => {
                   borderTop: "1px solid",
                   borderBottom: "1px solid",
                 }}
+                onClick={() => togglePlayPause(podcast.trackId)}
+                className="cursor-pointer"
               >
                 <td className="px-4 py-2 pt-4 pb-4">
                   {currentPlaying.audio &&
@@ -135,11 +150,12 @@ const PodcastList = ({ filteredPodcasts, isLoading }) => {
                       alt="podcast artwork"
                       className="mr-4 rounded-xl cursor-pointer"
                       style={{ width: "45px", height: "45px" }}
-                      onClick={() => togglePlayPause(podcast.trackId)}
                     />
                     <audio
                       ref={(el) => (audioRefs.current[podcast.trackId] = el)}
                       src={podcast.audioUrl}
+                      onTimeUpdate={(e) => setCurrentTime(e.target.currentTime)}
+                      onLoadedMetadata={(e) => setDuration(e.target.duration)}
                     ></audio>
                     <div>
                       <p>{podcast.trackName}</p>
@@ -166,6 +182,106 @@ const PodcastList = ({ filteredPodcasts, isLoading }) => {
               </tr>
             ))
           )}
+          <div
+            className="fixed bottom-0 left-0 w-full text-white"
+            style={{ backgroundColor: "#1A1A1A" }}
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                {currentPlaying.audio && (
+                  <div className="flex-shrink-0">
+                    <img
+                      src={currentlyPlayingPodcast?.artworkUrl100}
+                      alt="Currently Playing Podcast"
+                      className="h-28 w-28"
+                    />
+                  </div>
+                )}
+                <div className="ml-5">
+                  <h2 className="text-xl font-semibold">
+                    {currentlyPlayingPodcast?.trackName || "No Podcast Playing"}
+                  </h2>
+                  <p>{currentlyPlayingPodcast?.artistName}</p>
+                </div>
+              </div>
+              <div>
+                <div className="flex flex-1 gap-4 ml-5">
+                  <img
+                    src={Shuffle}
+                    alt="shuffle"
+                    style={{ color: "white" }}
+                    className="cursor-pointer object-contain"
+                  />
+                  <img
+                    src={StepBack}
+                    alt="step-back"
+                    style={{ color: "white" }}
+                    className="cursor-pointer object-contain"
+                  />
+
+                  {/* Play/Pause icon logic */}
+                  {currentPlaying.audio && currentPlaying.trackId ? (
+                    <CiPause1
+                      style={{ color: "white" }}
+                      className="cursor-pointer"
+                      onClick={() => {
+                        if (currentPlaying.trackId && currentPlaying.audio) {
+                          togglePlayPause(currentPlaying.trackId);
+                        }
+                      }}
+                    />
+                  ) : (
+                    <CiPlay1
+                      style={{ color: "white" }}
+                      className="cursor-pointer"
+                      onClick={() => {
+                        if (currentPlaying.trackId && currentPlaying.audio) {
+                          togglePlayPause(currentPlaying.trackId);
+                        }
+                      }}
+                    />
+                  )}
+
+                  <img
+                    src={StepForward}
+                    alt="step-forward"
+                    style={{ color: "white" }}
+                    className="cursor-pointer object-contain"
+                  />
+                  <img
+                    src={Loop}
+                    alt="loop"
+                    style={{ color: "white" }}
+                    className="cursor-pointer object-contain"
+                  />
+                </div>
+              </div>
+              {/* Podcast Progress Bar and Time */}
+              <div className="flex items-center ml-5">
+                <span className="mr-2">{formatTime(currentTime)}</span>
+                <div
+                  style={{ width: "32rem" }}
+                  className="relative h-1.5 bg-gray-600 rounded-full mr-2"
+                >
+                  <div
+                    className="absolute h-1.5 bg-white rounded-full"
+                    style={{ width: `${(currentTime / duration) * 100}%` }}
+                  ></div>
+                </div>
+                <span>{formatTime(duration)}</span>
+              </div>
+              {/* Volume Control */}
+              <div className="flex items-center mr-4">
+                <CiVolumeHigh size={24} className="mr-2 cursor-pointer" />
+                <div className="relative w-32 h-1.5 bg-gray-600 rounded-full">
+                  <div
+                    className="absolute h-1.5 bg-white rounded-full"
+                    style={{ width: "70%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </tbody>
       </table>
     </>
